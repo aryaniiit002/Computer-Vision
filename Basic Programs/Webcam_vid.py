389e4253bb8b0a0e380685ae0353be36b5464ba3
Aryan Bindal <55039523+aryaniiit002@users.noreply.github.com>
2020-06-07 10:36:28 +0530

    Update Webcam_vid.py

--- Basic Programs/Webcam_vid.py
+++ Basic Programs/Webcam_vid.py
@@ -20,8 +20,14 @@ import numpy as np
 import cv2 
 
 cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 while (True):
     ret, frame = cap.read()
+    if not ret:
+       print("Can't receive frame (stream end?). Exiting ...")
+       break
     cv2.imshow("frame", frame)
     key = cv2.waitKey(1)
     if key == 27:
