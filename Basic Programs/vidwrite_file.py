07ebeaecd514323c042896e609fa44c794b699a1
Aryan Bindal <55039523+aryaniiit002@users.noreply.github.com>
2020-06-08 08:42:14 +0530

    Update vidwrite_file.py

--- Basic Programs/vidwrite_file.py
+++ Basic Programs/vidwrite_file.py
@@ -1,4 +1,5 @@
 # Reading the Video and Writing into a File
+
 #The same code is used to read the video and write to a file, except when
 #each frame is read, a new parameter holds the read frame in a flip mode
 #and writes the flipped frame into another video file.
@@ -10,20 +11,25 @@ cap = cv.VideoCapture(0)
 
 # Define the codec and create VideoWriter object
 fourcc = cv.VideoWriter_fourcc(*'XVID')
-out = cv.VideoWriter('output.mp4v', fourcc, 28, (640, 480))
+out = cv.VideoWriter('output.mp4v', fourcc, 15, (1280,720))
 
 while cap.isOpened():
     ret, frame = cap.read()
-    if not ret:
+
+    if not ret: 
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    frame2 = cv.flip(frame, 0)
+
+    frame = cv.flip(frame, 1)
+    
     # write the flipped frame
-    cv.imshow('frame2',frame2)
-    cv.imshow('frame', frame)
-    out.write(frame2)
+    out.write(frame)
+    
+    cv.imshow('Video',frame)
+    
     if cv.waitKey(1) == 32:
         break
+
 # Release everything if job is finished
 cap.release()
 out.release()
