854f9bcbbc53697b85735c5155438f0490aa28ed
Aryan Bindal <55039523+aryaniiit002@users.noreply.github.com>
2020-06-20 10:50:03 +0530

    Update Simple_Image_Thresholding.py

--- Basic Programs/Thresholding/Simple_Image_Thresholding.py
+++ Basic Programs/Thresholding/Simple_Image_Thresholding.py
@@ -7,9 +7,11 @@
 
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-img= cv2.imread('./images/gradient.jpg',0)
-img= cv2.resize(img,(350,350))
+
+img= cv2.imread('gradient.jpg',0)
+#img= cv2.resize(img,(350,350))
 
 # applying different thresholding techniques on the input image 
 
@@ -20,14 +22,27 @@ _, th3 = cv2.threshold(img, 150, 255, cv2.THRESH_TRUNC) # If pixel value (<150) 
 _, th4 = cv2.threshold(img, 150, 255, cv2.THRESH_TOZERO)# If pixel value (<150) all values changed to 0(black) but if pixel values (>150) no change in pixel values
 _, th5 = cv2.threshold(img, 150, 255, cv2.THRESH_TOZERO_INV) #inverse of th4
 
+titles = ['Original','Th_Binary','Th_Binary_INV','Th_TRUNC','Th_TOZERO','Th_TOZERO_INV']
+images = [img, th1,th2,th3,th4,th5]
 
-cv2.imshow('Image',img)
-cv2.imshow('Th_Binary',th1)
-cv2.imshow('Th_Binary_INV',th2)
-cv2.imshow('Th_TRUNC',th3)
-cv2.imshow('Th_TOZERO',th4)
-cv2.imshow('Th_TOZERO_INV',th5)
+# Using matplotlib
+for i in range(6):
+    plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+
+plt.show()
+#this gives output_1.png
+
+             # OR another method
+
+#cv2.imshow('Image',img)
+#cv2.imshow('Th_Binary',th1)
+#cv2.imshow('Th_Binary_INV',th2)
+#cv2.imshow('Th_TRUNC',th3)
+#cv2.imshow('Th_TOZERO',th4)
+#cv2.imshow('Th_TOZERO_INV',th5)
 
 # De-allocate any associated memory usage
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
