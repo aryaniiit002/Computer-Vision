5d086735778648e9e29b59239ab3c26eb8aab1ee
Aryan Bindal <55039523+aryaniiit002@users.noreply.github.com>
2020-06-20 11:17:03 +0530

    Add files via upload

---
+++ Basic Programs/Mouse-Callback-Function/mouse_event3.py
@@ -0,0 +1,31 @@
+# Draw a line segment on image at "Mouse click positions" using OpenCV-Python
+
+import numpy as np
+import cv2
+
+def click_event(event,x,y,flags,params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img,(x,y),5,(0,0,255),-1)
+        point.append((x,y))
+
+        if len(point)>=2:
+            cv2.line(img,point[-1],point[-2],(255,0,0), 4)
+        cv2.imshow('image',img)
+
+# Press Space key to separate the line from the previous point
+        if cv2.waitKey(0) ==32:
+            point.clear()
+
+img=np.zeros((512,512,3),np.uint8)
+
+cv2.namedWindow("image")
+point=[]
+cv2.setMouseCallback('image',click_event)
+
+while True:
+    cv2.imshow("image",img)
+    if cv2.waitKey(0)==ord('q'):
+        break
+cv2.destroyAllWindows
+
+# Output-> output_3.png
---
+++ Basic Programs/Mouse-Callback-Function/mouse_event4.py
@@ -0,0 +1,39 @@
+# Create a window filled with the same color as the point on image at "Mouse click position"
+
+import cv2
+import numpy as np
+
+def click_event(event,x,y,flags,params):
+    
+    if event == cv2.EVENT_LBUTTONDOWN:
+
+        blue= img[y,x,0]
+        green= img[y,x,1]
+        red= img[y,x,2]
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        strBGR = str(blue) + ', '+ str(green)+ ', '+ str(red)
+        #print('BGR coordinates: ',strBGR)
+
+        cv2.putText(img, strBGR, (x, y), font, .5, (0, 255, 255), 2)
+
+        cv2.circle(img, (x,y), 4, (0,0,255), -1)
+
+        output_img=np.zeros((512,512,3),np.uint8)
+        output_img[:] = [blue,green,red]
+
+        cv2.imshow('Image',img)
+        cv2.imshow('O/P_image',output_img)
+
+
+img=cv2.imread('mypic2.jpg')
+img= cv2.resize(img, (600,600))
+
+cv2.imshow('Image',img)
+
+cv2.setMouseCallback('Image',click_event)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows
+
+# Output-> output_4.png
